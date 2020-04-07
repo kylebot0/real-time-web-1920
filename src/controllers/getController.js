42ad961579f85227329aab4feae1f3a3f96480c6
BuildTools <unconfigured@null.spigotmc.org>
2020-04-07 14:53:09 +0200

    Add randomsearch

--- src/controllers/getController.js
+++ src/controllers/getController.js
@@ -10,6 +10,8 @@ const spotifyApi = new SpotifyWebApi({
 });
 const scopes = ['user-read-private', 'user-read-email','playlist-modify-public','playlist-modify-private']
 
+const development = true
+
 async function overview(req, res) {
     // let token = await getToken(req, res)
     // console.log(token.access_token)
@@ -42,16 +44,48 @@ async function getToken(req, res) {
     const { access_token, refresh_token } = data.body;
     spotifyApi.setAccessToken(access_token);
     spotifyApi.setRefreshToken(refresh_token);
-    res.redirect('https://rtw-1920.herokuapp.com/party/');
-    // res.redirect('http://localhost:3000/party');
+    if(development) {
+      res.redirect('http://localhost:3000/party');
+    } else {
+      res.redirect('https://rtw-1920.herokuapp.com/party/');
+    }
   } catch(err) {
     res.redirect('/#/error/invalid token');
   }
    
 }
 
-async function chat(req, res) {
+function getRandomSearch() {
+  // A list of all characters that can be chosen.
+  const characters = 'abcdefghijklmnopqrstuvwxyz';
   
+  // Gets a random character from the characters string.
+  const randomCharacter = characters.charAt(Math.floor(Math.random() * characters.length));
+  let randomSearch = '';
+
+  // Places the wildcard character at the beginning, or both beginning and end, randomly.
+  switch (Math.round(Math.random())) {
+    case 0:
+      randomSearch = randomCharacter + '%';
+      break;
+    case 1:
+      randomSearch = '%' + randomCharacter + '%';
+      break;
+  }
+
+  return randomSearch;
+}
+
+
+async function chat(req, res) {
+  const randomOffset = Math.floor(Math.random() * 10000);
+  let results = await spotifyApi.searchTracks(getRandomSearch(), {limit: 50, offset: 50})
+  .then(result => {
+    return result
+  })
+  .catch(err => console.log(err))
+  req.session.trackList = results.body.tracks.items
+  console.log(results.body.tracks.items)
   res.render('./pages/chat', {
       title: '',
       data: req.session.userData
